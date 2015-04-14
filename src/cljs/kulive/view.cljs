1db7a0250f1edf11fb2117ce57b5646340e239fb
sooheon <tngjs0@gmail.com>
2015-04-14 14:07:44 +0900

    Stuff

--- src/cljs/kulive/view.cljs
+++ src/cljs/kulive/view.cljs
@@ -24,8 +24,7 @@
            :on-key-down #(if (= (.-which %) 13)
                            (do (re-frame/dispatch
                                 [:course-search-entered])
-                               (.preventDefault %)
-                               (.stopPropagation %))
+                               (.preventDefault %))
                            nil)
            :placeholder "Course ID, Title, Professor, etc..."}]]]])))
 
@@ -44,6 +43,7 @@
            ^{:key (key course)} [course-component course])]]])))
 
 (defn course-row-component [course]
+<<<<<<< HEAD
   [:tr
    [:td (get-in (val course) [:kr :name])]
    [:td (get-in (val course) [:kr :number])]
@@ -51,36 +51,50 @@
    [:td (str/join " " (get-in (val course) [:en :schedule]))]
    [:td (get-in (val course) [:kr :credit-hours])]
    [:td (get-in (val course) [:kr :classification])]])
+=======
+  (let [search-tokens (re-frame/subscribe [:search-tokens])]
+    (fn []
+      [:tr
+       [:td (get-in (val course) [:kr :name])]
+       [:td (get-in (val course) [:kr :number])]
+       [:td (get-in (val course) [:kr :professor])]
+       [:td (str/join " " (get-in (val course) [:kr :schedule]))]
+       [:td (str/replace (get-in (val course) [:kr :credit-hours]) " " "")]
+       [:td (get-in (val course) [:kr :classification])]])))
+>>>>>>> Stuff
 
 (defn courses-table-component []
-  (let [courses (re-frame/subscribe [:courses-to-display])]
+  (let [courses-to-display (re-frame/subscribe [:courses-to-display])]
     (fn []
-      [:table.u-full-width
-       [:thead [:tr
-                [:th "Name"]
-                [:th "ID"]
-                [:th "Professor"]
-                [:th "Schedule"]
-                [:th "Credits"]
-                [:th "Type"]]]
-       [:tbody
-        (for [course @courses]
-          ^{:key (get-in (val course) [:kr :number])}
-          [course-row-component course])]])))
+      [:div [:table.u-full-width
+             [:thead [:tr
+                      [:th "Name"]
+                      [:th "ID"]
+                      [:th "Professor"]
+                      [:th "Schedule"]
+                      [:th "Credits"]
+                      [:th "Class"]]]
+             [:tbody (if (= 1 (count @courses-to-display))
+                       {:style {:background "#f5f6f0"}})
+              (for [course @courses-to-display]
+                ^{:key (get-in (val course) [:kr :number])}
+                [course-row-component course])]
+             (if (= 0 (count @courses-to-display))
+               [:p.test "No classes to show"])]])))
 
 (defn my-courses-component []
   (let [my-courses (re-frame/subscribe [:my-courses])]
     (fn []
       (if (empty? @my-courses)
         [:p "No courses yet"]
-        [:p (str/join "\n" @my-courses)]))))
+        [:p (str/join " " @my-courses)]))))
 
 (defn home-page []
   [:div.container
    [:div.row
     [:div {:course "nine column" :style {:margin-top "2%"
                                          :margin-bottom "5%"}}
-     [:h2 "KU Live"]
+     [:h1 "KU Live"]
      [search-component]
      ;; [courses-component]
      [courses-table-component]
